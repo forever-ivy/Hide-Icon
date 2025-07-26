8ce430ef9bde5c91eeb19b3dd74e594b0e3efff1
forever-ivy <yezzzus@163.com>
2025-07-26 17:56:03 +0800

    Initial Commit

---
+++ Hide-Icon/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Hide-Icon
+//
+//  Created by Forever-ivy on 2025/7/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ Hide-IconTests/Hide_IconTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hide_IconTests.swift
+//  Hide-IconTests
+//
+//  Created by Forever-ivy on 2025/7/26.
+//
+
+import Testing
+@testable import Hide_Icon
+
+struct Hide_IconTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
